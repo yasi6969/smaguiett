3d7574df8595d14857edda97822879acbacae261
yasi6969 <santiagocn08@gmail.com>
2025-04-03 19:12:28 -0500

    script

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,3 +9,28 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db, collection, getDocs };
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig2 = {
+
+};
+
+const app2 = initializeApp(firebaseConfig2);
+const db2 = getFirestore(app2);
+
+export { db2, collection, getDocs };
+
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig3 = {
+
+};
+
+const app3 = initializeApp(firebaseConfig3);
+const db3 = getFirestore(app3);
+
+export { db3, collection, getDocs };
